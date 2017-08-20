ecddbb5253148fb7c54b85f43af7a9dc65819801
SmallSharky <kizzyol76@gmail.com>
2017-08-20 16:03:55 +0300

    Create lab_1.sql

---
+++ LAB_1/lab_1.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS lab_1;
+CREATE DATABASE lab_1;
+USE lab_1;
+CREATE TABLE `desc` ( `id` INT(15) NOT NULL AUTO_INCREMENT, `r_type` VARCHAR(50), `name` VARCHAR(50), PRIMARY KEY(`id`) );
+
+CREATE TABLE `point` ( `p_id` INT(15) NOT NULL AUTO_INCREMENT, `x` INT NOT NULL, `y` INT NOT NULL, PRIMARY KEY(`p_id`));
+
+CREATE TABLE `prof` ( `id` INT(15) NOT NULL, `p_id` INT(15) NOT NULL, `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+
+INSERT INTO `desc` (`id`, `name`, `r_type`) VALUES
+('1', 'name_1', 'normal'),
+('2', 'name_2', 'good'),
+('3', 'name_3', 'normal'),
+('4', 'name_4', 'bad'),
+('5', 'name_5', 'normal'),
+('6', 'name_6', 'neutral');
+
+
+SELECT * FROM `desc`;
