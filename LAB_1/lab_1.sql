b49332a223e8d8a2e75f5db7117415e5bc900494
SmallSharky <kizzyol76@gmail.com>
2017-08-27 16:53:04 +0300

    Написана хранимая процедура, но с ошибками

--- LAB_1/lab_1.sql
+++ LAB_1/lab_1.sql
@@ -7,6 +7,8 @@ CREATE TABLE `point` ( `p_id` INT(15) NOT NULL AUTO_INCREMENT, `x` INT NOT NULL,
 
 CREATE TABLE `prof` ( `id` INT(15) NOT NULL, `p_id` INT(15) NOT NULL, `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
 
+CREATE TABLE `ret` (`id` INT(15) NOT NULL, `name` VARCHAR(50), `a` INT NULL, PRIMARY KEY(`id`) );
+
 
 -- Создаем дескрипторы
 INSERT INTO `desc` (`id`, `name`, `r_type`) VALUES
@@ -40,18 +42,57 @@ INSERT INTO `prof` (`id`, `p_id`, `date`) VALUES
 ('6', '4', '2017-08-20 00:00:00'),
 ('2', '1', '2017-08-08 00:00:00');
 SELECT * FROM `prof`;
+  
 
-
--- Наша функция
-
-CREATE PROCEDURE `lab_1_f` (OUT name INT)
+CREATE PROCEDURE `p2` ()
+LANGUAGE SQL
+READS SQL DATA
+MODIFIES SQL DATA
+SQL SECURITY DEFINER
 BEGIN
-       SELECT id INTO @id FROM desc WHERE date>SUBDATE(CURRENT_TIMESTAMP, Interval '1' MONTH);
-SET name = @id;
-END;
+	DELETE FROM @ret;
+	INSERT INTO ret (`id`) 
+       		SELECT id FROM `desc`;	
+	DECLARE cursor CURSOR FOR SELECT id FROM `ret`;       /*Объявление курсора и его заполнение */  
+	DECLARE  CONTINUE HANDLER FOR NOT FOUND SET @ex=1;         /*Что делать, когда больше нет записей*/  
+	SET @ex=0;
+	OPEN cursor;                                /*Открыть курсор*/  
+		WHILE @ex=0 DO
+			SET @n=0;
+			SET @average=NULL;
+			FETCH cursor INTO @d_id;
+			IF @ex=0 THEN
+				
+				DECLARE cur_2 CURSOR FOR SELECT p_id FROM `prof` WHERE id=d_id AND date>SUBDATE(CURRENT_TIMESTAMP, Interval '1' MONTH);
+				DECLARE CONTINUE HANDLER FOR NOT FOUND SET @ex_2=1;
+				SET @ex_2=0;
+				WHILE @ex_2=0 DO
+					FETCH cur_2 INTO @x_val;
+					IF @ex_2=0 THEN
+						IF @average=NULL THEN
+							SET @average=0;
+						END IF;
 
-CALL lab_1_f(@ret);
-SELECT @ret;
+						SET @average=@average+@x_val;
+						SET @n=@n+1;
+					END IF;
+	
+				END WHILE;
+				CLOSE cur_2;
+				SELECT name INTO @d_name FROM `desc` WHERE id=@d_id;
+				UPDATE `ret` SET name=@d_name, a=@average WHERE id=@d_id;
+			END IF;
+		END WHILE;
+	CLOSE cursor;                               /*Закрыть курсор*/  
+	
+	SELECT * FROM ret;
+END//  
+
+DELIMITER ;
+
+CALL p2();
+
+
 
 
 
