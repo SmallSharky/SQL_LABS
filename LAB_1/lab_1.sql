1a8b3e1bf5800f372d3ac973999801bc53f1ce97
SmallSharky <kizzyol76@gmail.com>
2017-08-29 20:42:38 +0300

    Исправлены комментарии

--- LAB_1/lab_1.sql
+++ LAB_1/lab_1.sql
@@ -43,47 +43,47 @@ INSERT INTO `prof` (`id`, `p_id`, `date`) VALUES
 ('2', '1', '2017-08-08 00:00:00');
 SELECT * FROM `prof`;
   
-DELIMITER // Разделитель. Окончание операции.
-CREATE PROCEDURE `p2` ()// Создание процедуры.
-LANGUAGE SQL // Язык: "SQL"
-READS SQL DATA // Программа содержит инструкции, которые читают данные.
-MODIFIES SQL DATA // подпрограмма содержит инструкции, которые могут записывать данные.
-SQL SECURITY DEFINER // Характеристика SQL SECURITY может использоваться, чтобы определить, должна ли подпрограмма быть выполнена
+DELIMITER //--Разделитель. Окончание операции.
+CREATE PROCEDURE `p2` ()--Создание процедуры.
+LANGUAGE SQL --Язык: "SQL"
+READS SQL DATA --Программа содержит инструкции, которые читают данные.
+MODIFIES SQL DATA -- подпрограмма содержит инструкции, которые могут записывать данные.
+SQL SECURITY DEFINER -- Характеристика SQL SECURITY может использоваться, чтобы определить, должна ли подпрограмма быть выполнена
 BEGIN
-	DELETE FROM ret; // Удалить данные из временной таблицы 'ret'.
-	INSERT INTO ret (`id`) // Добавить во временную таблицу номаера id.
-       		SELECT id FROM `desc`; // Выдать значения 'id' из таблицы 'desc.
-	DECLARE cursor CURSOR FOR SELECT id FROM `ret`; //Объявление курсора.
-	DECLARE  CONTINUE HANDLER FOR NOT FOUND SET @ex=1;     //SET Используется для задания значения.
-	SET @ex=0; // Объявление счетчика ex типа int.
-	OPEN cursor; Начало работы курсора.                                 
-		WHILE @ex=0 DO // Начало работы цикла.
-			SET @n=0; // n - количество точек.
-			SET @average=NULL; // Average - Среднее арифметическое.
-			FETCH cursor INTO @d_id; //FETCH Сдвигает курсор в массиве.
+	DELETE FROM ret; -- Удалить данные из временной таблицы 'ret'.
+	INSERT INTO ret (`id`) -- Добавить во временную таблицу номаера id.
+       		SELECT id FROM `desc`; -- Выдать значения 'id' из таблицы 'desc.
+	DECLARE cursor CURSOR FOR SELECT id FROM `ret`; --Объявление курсора.
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET @ex=1;     --SET Используется для задания значения.
+	SET @ex=0; -- Объявление счетчика ex типа int.
+	OPEN cursor; --Начало работы курсора.                                 
+		WHILE @ex=0 DO -- Начало работы цикла.
+			SET @n=0; -- n - количество точек.
+			SET @average=NULL; -- Average - Среднее арифметическое.
+			FETCH cursor INTO @d_id; --FETCH Сдвигает курсор в массиве.
 			IF @ex=0 THEN
 				
 				DECLARE cur_2 CURSOR FOR SELECT p_id FROM `prof` WHERE id=d_id AND date>SUBDATE(CURRENT_TIMESTAMP, Interval '1' MONTH);
-				// Объявляется курсор для передвижения по ячейкам таблицы 'prof', где id = p_id. Интервал 1 месяц.
+				-- Объявляется курсор для передвижения по ячейкам таблицы 'prof', где id = p_id. Интервал 1 месяц.
 
 				DECLARE CONTINUE HANDLER FOR NOT FOUND SET @ex_2=1;
-				SET @ex_2=0;// Объявление переменной типа ex_2 = 0.
-				WHILE @ex_2=0 DO // Цикл для нахждения среднего арифметического.
-					FETCH cur_2 INTO @x_val;//Сдвигает курсор cur_2 в x_val.
-					IF @ex_2=0 THEN// Если ex_2 = 0, то 
-						IF @average=NULL THEN// Если среднее значение = NULL.
-							SET @average=0; // Среднее значение = нулю.
+				SET @ex_2=0;--Объявление переменной типа ex_2 = 0.
+				WHILE @ex_2=0 DO -- Цикл для нахждения среднего арифметического.
+					FETCH cur_2 INTO @x_val;--Сдвигает курсор cur_2 в x_val.
+					IF @ex_2=0 THEN-- Если ex_2 = 0, то 
+						IF @average=NULL THEN-- Если среднее значение = NULL.
+							SET @average=0; -- Среднее значение = нулю.
 						END IF;
 
-						SET @average=@average+@x_val;// Среднее значение = СР.ЗН + x_val.
-						SET @n=@n+1;// Прибавляется значение n.
-					END IF;  // Закончить оператор IF.
+						SET @average=@average+@x_val;-- Среднее значение = СР.ЗН + x_val.
+						SET @n=@n+1;-- Прибавляется значение n.
+					END IF;  --Закончить оператор IF.
 	
-				END WHILE; // Закончить цикл WHILE.
-				CLOSE cur_2; // Удалить cur_2.
-				SELECT name INTO @d_name FROM `desc` WHERE id=@d_id;// Выбрать d_name из таблицы 'desc', где id=d_id
-				UPDATE `ret` SET name=@d_name, a=@average WHERE id=@d_id; //Оператор UPDATE обновляет 
-				// таблицу 'ret'. 
+				END WHILE; -- Закончить цикл WHILE.
+				CLOSE cur_2; -- Удалить cur_2.
+				SELECT name INTO @d_name FROM `desc` WHERE id=@d_id;-- Выбрать d_name из таблицы 'desc', где id=d_id
+				UPDATE `ret` SET name=@d_name, a=@average WHERE id=@d_id; --Оператор UPDATE обновляет 
+				-- таблицу 'ret'. 
 			END IF;
 		END WHILE;
 	CLOSE cursor;                                 
